0642e40cb9a6f676b4329553919fdd5783bc82f3
Ian <wangezi0708@gamil.com>
2024-11-28 17:10:07 +0800

    add Vue

---
+++ Vue.js/app.js
@@ -0,0 +1,18 @@
+// Vue.js 2.x
+/* 
+var app = new app({
+
+// el 即 Element，Vue 以 css 的選擇器指定要 render 的位置
+   el : '#app',
+
+// Vue Render 時使用的資料
+   data: {
+   message: "Hello World",
+  }
+})
+*/
+
+// Vue.js 3.x
+var app = Vue.createApp(){
+    // 資料模型
+}
